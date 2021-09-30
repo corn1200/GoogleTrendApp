09e0052adf5bb927ace42bffe765b5825342fa7a
corn1200 <corn120033@gmail.com>
2021-09-30 15:48:33 +0900

    feat: 로딩 화면 생성

---
+++ app/src/main/java/com/example/googletrendapp/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.googletrendapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(mainIntent);
+        SplashActivity.this.finish();
+    }
+}
