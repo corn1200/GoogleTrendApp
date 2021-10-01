240626219f6af426e622e8a701f997e82adea34f
corn1200 <corn120033@gmail.com>
2021-10-01 16:05:26 +0900

    임시

--- app/src/main/java/com/example/googletrendapp/MainActivity.java
+++ app/src/main/java/com/example/googletrendapp/MainActivity.java
@@ -1,16 +1,139 @@
 package com.example.googletrendapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
+    private String htmlPageUrl = "https://trends.google.co.kr/trends/trendingsearches/daily?geo=KR";
+    private TextView textView;
+    private String htmlContentInStringFormat;
+    private WebView webView;
+    private String source;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar mainToolBar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(mainToolBar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        // 드로어 레이아웃 관련 동작 정의
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle
+                (this, drawer, mainToolBar,
+                        R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+        if (drawer != null) {
+            drawer.addDrawerListener(toggle);
+        }
+        toggle.syncState();
+
+        // 네비게이션뷰 관련 정의
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new JavascriptInterface(), "Android");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                view.loadUrl("javascript:window.Android.getHtml"
+                        + "(document.getElementsByTagName('body')[0].innerHTML);");
+            }
+        });
+        webView.loadUrl(htmlPageUrl);
+
+        textView = findViewById(R.id.textView);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+                jsoupAsyncTask.execute();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer != null) {
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                super.onBackPressed();
+            }
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return false;
+    }
+
+    private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPostExecute(Void unused) {
+//            super.onPostExecute(unused);
+            textView.setText(htmlContentInStringFormat);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Document doc = Parser.parse(source, htmlPageUrl);
+                Elements links = doc.select("span[ng-repeat] span");
+
+                for (Element link : links) {
+                    htmlContentInStringFormat += link.text().trim() + "\n";
+                }
+            } catch (Exception e) {
+                htmlContentInStringFormat += "error" + "\n";
+                Log.d("error", "error");
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    private class JavascriptInterface {
+        public void getHtml(String html) {
+            source = html;
+        }
     }
 }
